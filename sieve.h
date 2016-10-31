ef3ccb155376f760c92de8f04f4575b52432dc6b
total-recall <josef.steppan@gmail.com>
2016-10-31 09:14:31 +0100

    code cleanup

--- sieve.h
+++ sieve.h
@@ -2,11 +2,6 @@
 #define SIEVE_INVALID_ARGUMENT 1
 #define SIEVE_OUT_OF_MEMORY 2
 
-#define SetBit(A, k)     ( A[(k/32)] |= (1 << (k%32)) )
-#define ClearBit(A, k)   ( A[(k/32)] &= ~(1 << (k%32)) )
-#define TestBit(A, k)    ( A[(k/32)] & (1 << (k%32)) )
-
 typedef unsigned long long uint64_t;
 
 uint64_t *sieve(uint64_t limit, int *status, size_t *primeCount);
-
